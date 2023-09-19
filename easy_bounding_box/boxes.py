600a2223bdc8c87249375ebdf6df53cf911b0ede
LucasLCO <lucaslco2005@gmail.com>
2023-09-19 11:40:45 -0300

    feat: add boxes attr in BoundingBoxes

--- easy_bounding_box/boxes.py
+++ easy_bounding_box/boxes.py
@@ -1,6 +1,9 @@
 from typing import List
+from box import BoundingBox
 
 
 class BoundingBoxes:
     def __init__(self, boxes) -> None:
-        self.boxes = boxes
+        self.boxes = map(lambda x : BoundingBox(x), boxes)
+        
+        
